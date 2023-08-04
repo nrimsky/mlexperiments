b847485876f69337c621c968ea8cd777c9bc050f
nrimsky <ninarimsky@gmail.com>
2023-08-03 18:11:00 -0700

    Visualise embeddings

--- mlp_modular.py
+++ mlp_modular.py
@@ -1,6 +1,8 @@
 import torch as t
 import torch.nn as nn
 import torch.utils.data as data
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 class ModuloAdditionDataset(data.Dataset):
     def __init__(self, d_vocab=114):
@@ -37,6 +39,19 @@ class MLP(nn.Module):
         x = self.silu(x)
         return self.linear2(x)
     
+
+def plot_embeddings(model, vocab_size):
+    plt.clf()
+    embeddings = model.embedding.detach().cpu().numpy()
+    pca = PCA(n_components=2)
+    embeddings_2d = pca.fit_transform(embeddings)
+    plt.figure(figsize=(10, 10))
+    plt.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1])
+    words = [str(i) for i in range(vocab_size)] 
+    for i, word in enumerate(words):
+        plt.annotate(word, xy=(embeddings_2d[i, 0], embeddings_2d[i, 1]))
+    plt.savefig("embeddings.png")
+
 
 def train(vocab_size = 114, train_frac = 0.3, hidden_dim = 32, embed_dim = 16):
     model = MLP(vocab_size=vocab_size, embed_dim=embed_dim, hidden_dim=hidden_dim)
@@ -80,7 +95,12 @@ def train(vocab_size = 114, train_frac = 0.3, hidden_dim = 32, embed_dim = 16):
                     val_loss += loss.item()
                     val_acc += (output.argmax(dim=-1) == target).float().mean()
             print(f"Epoch {epoch}: train loss {train_loss / len(train_loader)}; test loss {val_loss / len(test_loader)}; test acc {val_acc / len(test_loader)}")
+    t.save(model.state_dict(), "modular_addition.ckpt")
 
 
 if __name__ == "__main__":
     train(vocab_size = 114, train_frac = 0.3, embed_dim = 12, hidden_dim = 32) #best so far embed_dim = 12, hidden_dim = 32
+    model = MLP(vocab_size=114, embed_dim=12, hidden_dim=32)
+    model.load_state_dict(t.load("modular_addition.ckpt"))
+    model.eval()
+    plot_embeddings(model, 114)
