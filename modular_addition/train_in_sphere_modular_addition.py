dec31989eb80e9f886ebd2b8ac28b5e43df9a547
nrimsky <ninarimsky@gmail.com>
2023-08-16 18:19:23 -0700

    Update gitignore

--- modular_addition/train_in_sphere_modular_addition.py
+++ modular_addition/train_in_sphere_modular_addition.py
@@ -8,64 +8,8 @@ from mlp_modular import (
     plot_embeddings_chunks,
 )
 import torch as t
-from torch.autograd import grad
-from scipy.sparse.linalg import LinearOperator, eigsh
-import numpy as np
 from torch.nn.utils import parameters_to_vector, vector_to_parameters
-
-
-def hessian_eig(
-    model,
-    loss_fn,
-    train_data_loader,
-    device="cuda",
-    n_top_vectors=200,
-    param_extract_fn=None,
-    reg=0.002
-):
-    param_extract_fn = param_extract_fn or (lambda x: x.parameters())
-    num_params = sum(p.numel() for p in param_extract_fn(model))
-    subset_a, subset_b, subset_res = [], [], []
-    for a, b, res in train_data_loader:
-        subset_a.append(a.to(device))
-        subset_b.append(b.to(device))
-        subset_res.append(res.to(device))
-    subset_a = t.cat(subset_a)
-    subset_b = t.cat(subset_b)
-    subset_res = t.cat(subset_res)
-
-    def compute_loss():
-        output = model(subset_a, subset_b)
-        return loss_fn(output, subset_res) + reg * get_weight_norm(model) # hacky way to add weight norm
-
-    def hessian_vector_product(vector):
-        model.zero_grad()
-        grad_params = grad(compute_loss(), param_extract_fn(model), create_graph=True)
-        flat_grad = t.cat([g.view(-1) for g in grad_params])
-        grad_vector_product = t.sum(flat_grad * vector)
-        hvp = grad(grad_vector_product, param_extract_fn(model), retain_graph=True)
-        return t.cat([g.contiguous().view(-1) for g in hvp])
-
-    def matvec(v):
-        v_tensor = t.tensor(v, dtype=t.float32, device=device)
-        return hessian_vector_product(v_tensor).cpu().detach().numpy()
-
-    linear_operator = LinearOperator((num_params, num_params), matvec=matvec)
-    eigenvalues, eigenvectors = eigsh(
-        linear_operator, k=n_top_vectors, tol=0.001, which="LM", return_eigenvectors=True
-    )
-    tot = 0
-    thresholds = [0.1, 1, 2, 10]
-    for e in eigenvalues:
-        print("{:.2f}".format(e))
-    for threshold in thresholds:
-        for e in eigenvalues:
-            if e > threshold:
-                tot += 1
-        print(f"Number of eigenvalues greater than {threshold}: {tot}")
-        tot = 0
-    eigenvectors = np.transpose(eigenvectors)
-    return tot, eigenvalues, eigenvectors
+from hessian_eig import hessian_eig_modular_addition
 
 
 def get_module_parameters(model):
@@ -210,7 +154,7 @@ def main(checkpoint_path = "modular_addition.ckpt"):
     train_loader, test_loader = get_train_test_loaders(
         train_frac=0.4, batch_size=256, vocab_size=VOCAB_SIZE
     )
-    _, _, eigenvectors = hessian_eig(
+    _, _, eigenvectors = hessian_eig_modular_addition(
         model,
         loss_fn,
         test_loader,
